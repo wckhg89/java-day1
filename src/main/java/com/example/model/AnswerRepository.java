46bb63816de4a1eec0ef6fba0ad2f01b6ace71a2
kanghonggu <kanghonggu@zuminternet.com>
2016-12-05 12:51:40 +0900

    답변달기 답변삭제 구현

---
+++ src/main/java/com/example/model/AnswerRepository.java
@@ -0,0 +1,9 @@
+package com.example.model;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Created by kanghonggu on 2016-12-05.
+ */
+public interface AnswerRepository extends JpaRepository<Answer, Long> {
+}
