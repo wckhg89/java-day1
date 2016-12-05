cae44411d8c06eb79164cd95acc7c744de1392b9
honggu <wckhg89@gmail.com>
2016-12-05 12:56:48 +0900

    --

---
+++ src/main/java/com/example/model/Answer.java
@@ -0,0 +1,7 @@
+package com.example.model;
+
+/**
+ * Created by 강홍구 on 2016-12-01.
+ */
+public class Answer {
+}
---
+++ src/main/java/com/example/model/AnswerRepository.java
@@ -0,0 +1,10 @@
+package com.example.model;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Created by 강홍구 on 2016-12-01.
+ */
+public interface AnswerRepository extends JpaRepository<Answer, Long> {
+
+}
